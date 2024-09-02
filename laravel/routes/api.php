d5eaeb5c435146c39c4b758517957e255740bcf0
SashaMagDEv <magnuh10@gmail.com>
2024-09-02 13:14:13 +0300

    changes in the project

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -17,25 +17,25 @@ use App\Http\Controllers\CategoryController;
 
 Route::group(['prefix' => 'categories'], function () {
     Route::get('/', [CategoryController::class, 'index']);
+    Route::group(['prefix' => '{category_id}'], function () {
+        Route::post('/news', [NewsController::class, 'store']);
+    });
     Route::group(['prefix' => '{id}'], function () {
         Route::get('/', [CategoryController::class, 'show']);
         Route::get('/news', [CategoryController::class, 'getNewsByCategory']);
 
     });
 });
-Route::post('/categories/{category_id}/news', [NewsController::class, 'store']);
-
-
 Route::group(['prefix' => 'news'], function () {
     Route::get('/', [NewsController::class, 'index']);
     Route::post('/', [NewsController::class, 'store']);
     Route::group(['prefix' => '{id}'], function () {
         Route::get('/', [NewsController::class, 'show']);
-        Route::get('/edit', [NewsController::class, 'show']);
         Route::put('/', [NewsController::class, 'update']);
         Route::delete('/', [NewsController::class, 'deleteNews']);
     });
 });
+
 
 Route::middleware('auth:sanctum')->get('/user', function (\App\Http\Requests\NewsRequest $request) {
     return $request->user();
